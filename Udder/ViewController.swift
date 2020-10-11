47b8c7e5768933dbabb4e4a7ec6a442386e227bd
Christos Lianos <45062430+Christoslianos@users.noreply.github.com>
2020-10-11 10:08:39 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Christos Lianos on 2020-10-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
